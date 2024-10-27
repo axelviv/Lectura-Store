7d0d30490e421684e6db8bfa0c36f8e4e34ae03d
axelviv <axelviv@live.com.ar>
2024-10-26 22:29:52 -0300

    TP nº4

--- Pages/index.js
+++ Pages/index.js
@@ -2,10 +2,9 @@
 /* Components */
 import { navBarComponent } from "../Components/navBar.js"
 import { cardComponent } from "../Components/cards.js"
-import { cardsElementsLibros } from "../Components/cardsElementsLibros.js"
-import { cardsElementsSeñaladores } from "../Components/cardsElementsSeñaladores.js"
-import { cardsElementsFundas } from "../Components/cardsElementsFundas.js"
+import { cardHomeComponent } from "../Components/cardsHome.js"
 import { cerrarSesion} from "./users/logout.js"
+
 
 /* NavBar */
 let navContainer = document.querySelector('header')
@@ -14,30 +13,52 @@ window.addEventListener('load', ()=>{
     navContainer.innerHTML = navBarComponent
 })
 
+
 /* Cerrar Sesion */
 cerrarSesion();
 
+
 /* -----------------------Cards----------------------------- */
-
-/* Libros */
 let cardContainerLibros = document.querySelector('#card-libros')
-
-window.addEventListener('load', ()=>{
-    cardContainerLibros.innerHTML = cardComponent(cardsElementsLibros)
-})
-
-/* Señaladores */
 let cardContainerSeñaladores = document.querySelector('#card-señaladores')
-
-window.addEventListener('load', ()=>{
-    cardContainerSeñaladores.innerHTML = cardComponent(cardsElementsSeñaladores)
-})
-
-/* Fundas */
 let cardContainerFundas = document.querySelector('#card-fundas')
 
+let cardHomeContainerLibros = document.querySelector('#cardHome-libros')
+let cardHomeContainerSeñaladores = document.querySelector('#cardHome-señaladores')
+let cardHomeContainerFundas = document.querySelector('#cardHome-fundas')
+
 window.addEventListener('load', ()=>{
-    cardContainerFundas.innerHTML = cardComponent(cardsElementsFundas)
+    
+    fetch("../Components/productos.json").then(res => res.json()).then(dat => {
+        
+        //Secciones de Productos
+        //Se encuentran dentro de if porque el codigo se cortaba cuando no encontraba uno de los id
+        if(cardContainerLibros){
+            cardContainerLibros.innerHTML = cardComponent(dat.cardsElementsLibros)
+        }
+
+        if(cardContainerSeñaladores){
+            cardContainerSeñaladores.innerHTML = cardComponent(dat.cardsElementsSeñaladores)
+        }
+
+        if(cardContainerFundas){
+            cardContainerFundas.innerHTML = cardComponent(dat.cardsElementsFundas)
+        }
+
+        //Productos en Home
+        if(cardHomeContainerLibros){
+            cardHomeContainerLibros.innerHTML = cardHomeComponent(dat.cardsElementsLibros.slice(0, 3))
+        }
+
+        if(cardHomeContainerSeñaladores){
+            cardHomeContainerSeñaladores.innerHTML = cardHomeComponent(dat.cardsElementsSeñaladores.slice(0, 3))
+        }
+
+        if(cardHomeContainerFundas){
+            cardHomeContainerFundas.innerHTML = cardHomeComponent(dat.cardsElementsFundas.slice(0, 3))
+        }
+
+    }).catch(error =>{
+        console.log(error)
+    })
 })
-
-
